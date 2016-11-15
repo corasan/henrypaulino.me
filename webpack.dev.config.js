7c94f1fe07f1348e114ffc244834690574137045
Henry Paulino <henrypl360@gmail.com>
2016-11-15 03:10:27 -0500

    new js bundle, fix

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,12 +2,19 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.js',
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
