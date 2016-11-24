4561a3a31a48a42cf8b2c9a4297c2e1f9109e448
Henry Paulino <henrypl360@gmail.com>
2016-11-24 12:07:20 -0500

    about me styles, latest build

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <div>About page</div>
+      <div className="about-me">
+        <div className="heading">
+          <h1>About me</h1>
+        </div>
+
+        <div className="about-me-content">
+
+        </div>
+      </div>
     );
   }
 }
