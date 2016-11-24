465ddd53b3518d07e8b322a574f86135113e2b2e
Henry Paulino <henrypl360@gmail.com>
2016-11-24 02:41:36 -0500

    changes

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import _ from 'lodash';
 
 export default class Navigator extends Component {
-  componentDidMount() {
-
-  }
-  
   render() {
     return (
       <div className="nav">
         <ul>
-          <li><Link activeClassName="active">About</Link></li>
-          <li><Link activeClassName="active">Projects</Link></li>
+          <li><Link activeClassName="active" className="hover-link">About</Link></li>
+          <li><Link activeClassName="active" className="hover-link">Projects</Link></li>
         </ul>
       </div>
     );
