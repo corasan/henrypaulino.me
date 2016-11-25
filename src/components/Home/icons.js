81bf62f7a5e215d701f40348d8a902c9c568054c
Henry Paulino <henrypl360@gmail.com>
2016-11-25 12:05:27 -0500

    fix typo for github icon

--- src/components/Home/icons.js
+++ src/components/Home/icons.js
@@ -4,12 +4,12 @@ export default class Icons extends Component {
   componentDidMount() {
     TweenMax.from('#icons', 0.4, {opacity: 0, delay: 3});
   }
-  
+
   render() {
     return (
       <div id="icons">
         <img src="../../../dist/icons/Behance_2.svg" className="icon"/>
-        <img src="../../../dist/icons/Github.svg" className="icon"/>
+        <img src="../../../dist/icons/GitHub.svg" className="icon"/>
         <img src="../../../dist/icons/LinkedIn.svg" className="icon"/>
         <img src="../../../dist/icons/Twitter.svg" className="icon"/>
         <img src="../../../dist/icons/WordPress.svg" className="icon"/>
