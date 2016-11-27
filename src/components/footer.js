4eaa5c40d79570691a4aa7890a7da4c45588b1aa
Henry Paulino <henrypl360@gmail.com>
2016-11-27 14:14:20 -0500

    change icons img src to aws S3

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { footerAnimation } from '../animations';
 
-const iconPath = process.env.NODE_ENV === 'production' ? '/icons' : '../../dist/icons';
-
 export class Icons extends Component {
   componentDidMount() {
     footerAnimation('#icons-group');
@@ -15,19 +13,19 @@ export class Icons extends Component {
     return (
       <div id="icons-group">
         <a target="_blank" href="https://www.behance.net/corasan360">
-          <img src={`${iconPath}/Behance_2.png`} className="icon"/>
+          <img src="https://s3.amazonaws.com/henrypaulino/icons/Behance_2.png" className="icon"/>
         </a>
         <a target="_blank" href="https://github.com/corasan">
-          <img src={`${iconPath}/GitHub.png`} className="icon"/>
+          <img src="https://s3.amazonaws.com/henrypaulino/icons/GitHub.png" className="icon"/>
         </a>
         <a target="_blank" href="https://www.linkedin.com/in/henrypl95">
-          <img src={`${iconPath}/LinkedIn.png`} className="icon"/>
+          <img src="https://s3.amazonaws.com/henrypaulino/icons/LinkedIn.png" className="icon"/>
         </a>
         <a target="_blank" href="https://twitter.com/henrypl95">
-          <img src={`${iconPath}/Twitter.png`} className="icon"/>
+          <img src="https://s3.amazonaws.com/henrypaulino/icons/Twitter.png" className="icon"/>
         </a>
         <a target="_blank" href="https://henrypl95.wordpress.com/">
-          <img src={`${iconPath}/WordPress-50.png`} className="icon"/>
+          <img src="https://s3.amazonaws.com/henrypaulino/icons/WordPress-50.png" className="icon"/>
         </a>
       </div>
     )
