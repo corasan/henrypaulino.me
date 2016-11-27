679c560b2ae264a2b59071e4e0ce9c1e50a18a68
Henry Paulino <henrypl360@gmail.com>
2016-11-27 13:47:18 -0500

    animation change

--- src/animations.js
+++ src/animations.js
@@ -1,7 +1,7 @@
 export const enterTransition = (target) => {
-  TweenMax.from(target, 1.5, {opacity: 0, x: 150});
+  TweenMax.from(target, 1.8, {opacity: 0, x: 160});
 }
 
 export const footerAnimation = (target) => {
-  TweenMax.from(target, 0.8, {opacity: 0});
+  TweenMax.from(target, 1.2, {opacity: 0});
 }
