8ca4635510225c599c937080b459cbe00921150a
Henry Paulino <henrypl360@gmail.com>
2016-11-28 23:12:39 -0500

    separate contact form from main component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { enterTransition } from '../../animations.js';
+import ContactForm from './contactForm';
 
 export default class Contact extends Component {
+  componentDidMount() {
+    enterTransition('.contact-me');
+  }
+
+  componentWillUnmount() {}
+
   render() {
     return (
       <div className="contact-me">
@@ -8,19 +16,7 @@ export default class Contact extends Component {
           <h1>Contact me</h1>
         </div>
 
-        <div>
-          <form action="mailto:henrypl360@gmail.com" method="post" enctype="text/plain" className="contact-form">
-            <div className="column-input">
-              <input type="text" name="name" placeholder="Name"/><br/>
-              <input type="text" name="subject" placeholder="Subject"/><br/>
-              <input type="email" name="email" placeholder="Email"/><br/>
-            </div>
-
-            <div className="column-textarea">
-              <textarea type="text" name="message" rows="6" cols="50" placeholder="Message..."/>
-            </div>
-          </form>
-        </div>
+        <ContactForm/>
       </div>
     );
   }
---
+++ src/components/Contact/contactForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+export default class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      subject: '',
+      email: '',
+      message: ''
+    }
+  }
+
+  handleName = (e) => this.setState({name: e.target.value});
+
+  handleSubject = (e) => this.setState({subject: e.targer.value});
+
+  handleEmail = (e) => this.setState({email: e.target.value});
+
+  handleMessage = (e) => this.setState({message: e.target.value});
+
+  render() {
+    return (
+      <div>
+        <form action="mailto:henrypl360@gmail.com" method="post" encType="text/plain">
+          <div className="contact-form">
+            <div className="column-input">
+              <input type="text" name="name" placeholder="Name" onChange={this.handleName}
+              value={this.state.name}/><br/>
+              <input type="text" name="subject" placeholder="Subject" onChange={this.handleSubject}/><br/>
+              <input type="email" name="email" placeholder="Email" onChange={this.handleEmail}
+              value={this.state.email}/><br/>
+            </div>
+
+            <div className="column-textarea">
+              <textarea type="text" name="message" rows="6" cols="50" placeholder="Message..."
+              onChange={this.handleMessage} value={this.state.message}/><br/>
+            </div>
+          </div>
+
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    );
+  }
+}
